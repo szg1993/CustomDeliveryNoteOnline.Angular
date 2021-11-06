980ab3b22d1cbcfde52d80acc18820f60579a7b1
Gábor Sztárcsevics <szg1993@gmail.com>
2021-11-06 19:59:15 +0100

    Recipient upsert form added.

--- src/app/recipient/components/recipient-upsert.component.ts
+++ src/app/recipient/components/recipient-upsert.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipient-upsert.component.css']
 })
 export class RecipientUpsertComponent implements OnInit {
-
+  pageTitle: string = 'Create Recipient';
   constructor() { }
 
   ngOnInit(): void {
